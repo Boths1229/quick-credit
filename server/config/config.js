01e18c8209a308ae02307c0a686f11d9a0ff26cc
chukwuma Emmanuel <boths1229@gmail.com>
2019-05-14 11:24:58 +0100

    bug(fix): fix my UI user experince

--- server/config/config.js
+++ server/config/config.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const config = {
-  port: process.env.PORT,
+  port: process.env.PORT || 3000,
   env: process.env.NODE_ENV,
   databaseUrl: process.env.DB_URL,
 };
